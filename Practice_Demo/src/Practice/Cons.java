57d0eeba60632d2908ca34300bbfcc316330e887
zuberdiwan <zuberdiwan@DESKTOP-82FRGBR>
2021-03-18 16:48:43 +0530

    New added

---
+++ Practice_Demo/src/Practice/Cons.java
@@ -0,0 +1,14 @@
+package Practice;
+
+public class Cons {
+	Cons() {
+	System.out.println("HEllo");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cons c=new Cons();
+
+	}
+
+}
---
+++ Practice_Demo/src/Practice/PatternProgram.java
@@ -0,0 +1,16 @@
+package Practice;
+
+public class PatternProgram {
+	public static void main(String[] args) {
+		int rows = 5;
+
+		for(int i=1;i<=rows;++i) {
+				
+			for(int j=1;j<=i;++j) {
+				System.out.print("*");
+			}
+			 System.out.println();
+			}
+		   
+	}
+}
