b9bf8719e29468230ceedd4c490f15dc5abf011c
zuberdiwan <47203760+zuberdiwan@users.noreply.github.com>
2021-03-18 15:10:18 +0530

    Update Helloword.java

--- Practice_Demo/src/Practice/Helloword.java
+++ Practice_Demo/src/Practice/Helloword.java
@@ -2,6 +2,6 @@ package Practice;
 
 public class Helloword {
 public static void main(String[] args) {
-	System.out.println("This is my Git Work");
+	System.out.println("hello zuber your update work fine...");
 }
 }
