3fd218bbe758a9ce3bb152b8c46713abcf29a1b7
zuberdiwan <47203760+zuberdiwan@users.noreply.github.com>
2021-03-18 15:12:02 +0530

    Update Helloword.java

--- Practice_Demo/src/Practice/Helloword.java
+++ Practice_Demo/src/Practice/Helloword.java
@@ -2,6 +2,6 @@ package Practice;
 
 public class Helloword {
 public static void main(String[] args) {
-	System.out.println("hello zuber your update work fine...");//comment from zuber diwan
+	System.out.println("hello zuber your update work fine...");//comment from zuber 
 }
 }
